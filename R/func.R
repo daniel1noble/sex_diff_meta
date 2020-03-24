5b6c9f41afcbafaf36b10b9700109907d43da160
Daniel Noble <daniel.wa.noble@gmail.com>
2020-03-24 11:50:19 +1100

    Added some new phylogeny code. Still problems with phylogeny spevies names not matching

--- R/func.R
+++ R/func.R
@@ -7,10 +7,11 @@
 # This function will fit multi-level meta-analytic models (intercept only) to estimate the overall effect on the mean and CV accounting for study and species random effects and estimating a residual error (within study/species) variance. 
 
 #TO DO: Incorporate the phylogeny when the phylogenies are corrected and available
-fit_int_MLMAmodels <- function(data){
-          lnCVR <- metafor::rma.mv(CVR_yi ~ 1, V = CVR_vi, random = list(~1|study_ID, ~1|species_name, ~1|obs), data = data)
+fit_int_MLMAmodels <- function(data, phylo_vcv){
 
-            SMD <- metafor::rma.mv(SMD_yi ~ 1, V = SMD_vi, random = list(~1|study_ID, ~1|species_name, ~1|obs), data = data) 
+          lnCVR <- metafor::rma.mv(CVR_yi ~ 1, V = CVR_vi, random = list(~1|study_ID, ~1|spp, ~1|obs), R = list(spp=phylo_vcv), data = data)
+
+            SMD <- metafor::rma.mv(SMD_yi_flip ~ 1, V = SMD_vi, random = list(~1|study_ID, ~1|spp, ~1|obs), R = list(spp=phylo_vcv), data = data) 
 
           return(list(SMD = SMD, 
           			lnCVR = lnCVR))
@@ -19,7 +20,7 @@ fit_int_MLMAmodels <- function(data){
 fit_MLMA_reg_models_personality_trait <- function(data){
           lnCVR <- metafor::rma.mv(CVR_yi ~ personality_trait, V = CVR_vi, random = list(~1|study_ID, ~1|species_name, ~1|obs), data = data)
 
-            SMD <- metafor::rma.mv(SMD_yi ~ personality_trait, V = SMD_vi, random = list(~1|study_ID, ~1|species_name, ~1|obs), data = data) 
+            SMD <- metafor::rma.mv(SMD_yi_flip ~ personality_trait, V = SMD_vi, random = list(~1|study_ID, ~1|species_name, ~1|obs), data = data) 
 
           return(list(SMD = SMD, 
           			lnCVR = lnCVR))
@@ -28,7 +29,7 @@ fit_MLMA_reg_models_personality_trait <- function(data){
 fit_MLMA_reg_models_personality_trait_SSD_index <- function(data){
           lnCVR <- metafor::rma.mv(CVR_yi ~ personality_trait*SSD_index, V = CVR_vi, random = list(~1|study_ID, ~1|species_name, ~1|obs), data = data)
 
-            SMD <- metafor::rma.mv(SMD_yi ~ personality_trait*SSD_index, V = SMD_vi, random = list(~1|study_ID, ~1|species_name, ~1|obs), data = data) 
+            SMD <- metafor::rma.mv(SMD_yi_flip ~ personality_trait*SSD_index, V = SMD_vi, random = list(~1|study_ID, ~1|species_name, ~1|obs), data = data) 
 
           return(list(SMD = SMD, 
           			lnCVR = lnCVR))
@@ -37,7 +38,7 @@ fit_MLMA_reg_models_personality_trait_SSD_index <- function(data){
 fit_MLMA_reg_models_personality_trait_mating_system  <- function(data){
           lnCVR <- metafor::rma.mv(CVR_yi ~ personality_trait*mating_system, V = CVR_vi, random = list(~1|study_ID, ~1|species_name, ~1|obs), data = data)
 
-            SMD <- metafor::rma.mv(SMD_yi ~ personality_trait*mating_system, V = SMD_vi, random = list(~1|study_ID, ~1|species_name, ~1|obs), data = data) 
+            SMD <- metafor::rma.mv(SMD_yi_flip ~ personality_trait*mating_system, V = SMD_vi, random = list(~1|study_ID, ~1|species_name, ~1|obs), data = data) 
 
           return(list(SMD = SMD, 
           			lnCVR = lnCVR))
@@ -46,20 +47,20 @@ fit_MLMA_reg_models_personality_trait_mating_system  <- function(data){
 fit_MLMA_reg_models_parental_care  <- function(data){
           lnCVR <- metafor::rma.mv(CVR_yi ~ parental_care, V = CVR_vi, random = list(~1|study_ID, ~1|species_name, ~1|obs), data = data)
 
-            SMD <- metafor::rma.mv(SMD_yi ~ parental_care, V = SMD_vi, random = list(~1|study_ID, ~1|species_name, ~1|obs), data = data) 
+            SMD <- metafor::rma.mv(SMD_yi_flip ~ parental_care, V = SMD_vi, random = list(~1|study_ID, ~1|species_name, ~1|obs), data = data) 
 
           return(list(SMD = SMD, 
           			lnCVR = lnCVR))
 }
 
 # This function takes the dataset, splits the dataset up by broad taxonomic group and applies a specified meta-analytic or meta-regression model to each taxa, saving the model results into a list that can be used down stream
-meta_model_fits <- function(data, type = c("int", "pers", "pers_SSD", "pers_mate", "parent_care")){
-         taxa_list <- split(pers_new, pers_new$taxo_group)
+meta_model_fits <- function(data, phylo_vcv, type = c("int", "pers", "pers_SSD", "pers_mate", "parent_care")){
+         taxa_list <- split(data, data$taxo_group)
 
-      tyep <-  match.arg(type)
+      type <-  match.arg(type)
 
     if(type == "int"){
-        model_fits <- lapply(taxa_list, function(x) fit_int_MLMAmodels(x))
+        model_fits <- mapply(function(x, y) fit_int_MLMAmodels(x, y), x = taxa_list, y = phylo_vcv)
     }
 
     if(type == "pers"){
@@ -81,4 +82,18 @@ meta_model_fits <- function(data, type = c("int", "pers", "pers_SSD", "pers_mate
    return(model_fits)
 } 
 
+tree_checks <- function(data, tree){
+        # How many unqiue species exist in data and tree
+        Numbers <- matrix(,nrow = 2, ncol = 2)
+        Numbers[1,2] <- length(unique(data$spp)) #108
+        Numbers[2,2] <- length(tree$tip.label) #109
 
+        Numbers[,1] <- c("Species in data:", "Species in tree:")
+
+        # Missing species or species not spelt correct
+        #species_list <- tree$tip.label[!tree$tip.label %in% unique(data$spp)]        
+        species_list1= setdiff(sort(tree$tip.label), sort(unique(data$spp)))
+        species_list2= setdiff(sort(unique(data$spp)), sort(tree$tip.label) )
+
+        return(list(SpeciesNumbers = Numbers, SpeciesList_NotFound_InData_But_NotTree=species_list1, SpeciesList_NotFound_InTree_But_NOTData=species_list2))
+      }
