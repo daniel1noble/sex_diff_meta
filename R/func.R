5fa340f9b69c64e8a32e5740e1a9a173dec7c990
Daniel Noble <daniel.wa.noble@gmail.com>
2020-03-24 21:09:45 +1100

    Updated function and some new code needed with the functions updates. Can prune trees directly with this then when the checks are sufficiently good

--- R/func.R
+++ R/func.R
@@ -82,7 +82,9 @@ meta_model_fits <- function(data, phylo_vcv, type = c("int", "pers", "pers_SSD",
    return(model_fits)
 } 
 
-tree_checks <- function(data, tree, dataCol){
+tree_checks <- function(data, tree, dataCol, type = c("checks", "prune")){
+        type = match.arg(type)
+
         # How many unique species exist in data and tree
         Numbers <- matrix(nrow = 2, ncol = 1)
         Numbers[1,1] <- length(unique(data$spp_name_phylo)) 
@@ -94,9 +96,17 @@ tree_checks <- function(data, tree, dataCol){
         species_list1= setdiff(sort(tree$tip.label), sort(unique(data[,dataCol])))
         species_list2= setdiff(sort(unique(data[,dataCol])), sort(tree$tip.label) )
 
-        return(list(SpeciesNumbers = data.frame(Numbers), 
+        if(type == "checks"){
+          return(list(SpeciesNumbers = data.frame(Numbers), 
                     Species_InTree_But_NotData=species_list1, 
                     Species_InData_But_NotTree=species_list2))
+          }
+        
+        if(type == "prune"){
+            if(length(species_list2) >=1) stop("Sorry, you can only prune a tree when you have no taxa existing in the data that are not in the tree")
+
+              return(ape::drop.tip(tree, species_list1))
+              }
       }
 
 
